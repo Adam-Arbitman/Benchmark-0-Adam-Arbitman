285b97566563fe4567fe7e7d77236d4abda1770e
Adam-Arbitman <Adama258@nycstudents.net>
2025-12-15 10:36:53 -0500

    Create Stoplight.py

---
+++ Stoplight.py
@@ -0,0 +1,32 @@
+from tkinter import *
+#need to install on all machines
+from tkmacosx import Button
+
+# Create the main window
+root = Tk()
+root.title("Enter Title Here")
+
+#Set size of window
+root.geometry("300x150")
+
+# Create buttons
+red_button = Button(root, text="Red", background='red')
+
+#Add a label
+label = Label(root, text="CHANGE ME!")
+
+# Place widgets in window (with pack function!)
+label.pack()
+red_button.pack()
+
+
+# Start the GUI event loop
+root.mainloop()
+
+
+yellow_button = Button(root, text="Yellow", background='yellow')
+yellow_button.pack()
+
+
+green_button = Button(root, text="Blue", background='blue')
+green_button.pack()
